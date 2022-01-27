d4585e8a0e3bd75bfc6951b8033d79f5f0c57668
BISINIGISHIVANI <90177603+BISINIGISHIVANI@users.noreply.github.com>
2022-01-27 19:06:05 +0530

    nav bar updated

--- 7.dialogue styles/index.js
+++ 7.dialogue styles/index.js
@@ -8,3 +8,14 @@ function myFunction(){
     }
 }
 inputOne.addEventListener("click",myFunction) 
+var btnAlert=document.querySelector("#btn-alert");
+var alertAct=()=>{
+    var alertOut=document.querySelector(".dialog-act");
+    if(alertOut.style.display==="none"){
+        alertOut.style.display="block";
+    }else{
+        alertOut.style.display="none"
+    }
+}
+
+btnAlert.addEventListener("click",alertAct)
